49b93931fa4b3f91e7889e7060b5e8821958e506
Ilana Radinsky <ilanaradinsky@gmail.com>
2017-08-06 17:30:11 +0000

    Shiny app, more figures

--- shiny_apps/school_networks_shiny/server.R
+++ shiny_apps/school_networks_shiny/server.R
@@ -7,35 +7,215 @@
 
 library(shiny)
 library(tidyverse)
+library(networkD3)
+library(htmlwidgets)
+library(readr)
+library(Matrix)
+library(stringr)
 
-data(MisLinks)
-data(MisNodes)
+load('cooccur_matrix.Rdata')
+load('schoolcodemapping_fuller.Rdata')
+codemap <- percode_schoolmapping_fuller
+school_codemap <- data.frame(codemap$pgcode,codemap$school_name)
+colnames(school_codemap) <- c("pgcode","schoolname")
 
-input <- c()
-input$school <- "Valjean"
+clickJS <- '
+d3.selectAll(".node").on("click",
+    function(d){
+      Shiny.unbindAll();
+      var $select = $(\'#school\').selectize();
+      $select[0].selectize.setValue(d.name);
+      Shiny.bindAll()
+    })
+'
+get_related_nodes <- function(code) {
+  if(code!="") {
+    
+    code <-
+      codemap %>%
+      mutate(display_name=paste(as.character(pgname), " (", as.character(pgcode), ")", sep="")) %>%
+      filter(display_name==code)
+    
+    code <- as.character(code[1, "pgcode"])
+    
+    v <- C[code, which(C[code, ] > 2)]
+    v <-  tail(sort(v),11)
+    
+    co_occur<- C[names(v),names(v)]
+    
+    ### now plot things!
+    # convert the matrix back to a long dataframe
+    # first, grab indicies
+    network <- as.data.frame(which(co_occur!=0,arr.ind = T))
+    df <- as.data.frame(network)
+    
+    # next, get edge weights
+    rownames(df) <- NULL
+    df$weight <- co_occur[which(co_occur!=0)]
+    
+    # make things zero indexed so there's no complaining
+    df<- df %>%
+      filter(row < col) %>%
+      mutate(row = row - 1,
+             col = col - 1)
+    
+    # and provide nice names
+    names(df) <- c("source", "target", "value")
+    
+    #mapping school names to the program codes 
+    df_mat <- data.frame(names(v),v[names(v)])
+    
+    colnames(df_mat)<-c("pgcode","value")
+    rownames(df_mat)<- NULL
+    final <- merge(df_mat,percode_schoolmapping_fuller, by ="pgcode") %>%
+      mutate(display_name=paste(as.character(pgname), " (", as.character(pgcode), ")", sep=""))
+    
+    # add node labels and sizes
+    nodes<- data.frame(name = final$display_name,
+                       group = 1,
+                       size = diag(co_occur),
+                       dbn = final$dbn,
+                       school_name = final$school_name)
+    
+    
+    # same thing for program codes only
+    # nodes<- data.frame(name = final$pgcode,
+    #                    group = 1,
+    #                    size = diag(co_occur))
+    # 
+    nodes <- nodes %>% 
+      mutate(group = ifelse(name==code,1,2))
+    
+    return(nodes)
+  }
+}
+
+display_school_neighborhood <- function(code){
+  if(code!="") {
+    
+    code <-
+      codemap %>%
+      mutate(display_name=paste(as.character(pgname), " (", as.character(pgcode), ")", sep="")) %>%
+      filter(display_name==code)
+    
+    code <- as.character(code[1, "pgcode"])
+    
+    v <- C[code, which(C[code, ] > 2)]
+    v <-  tail(sort(v),10)
+  
+    co_occur<- C[names(v),names(v)]
+  
+    ### now plot things!
+    # convert the matrix back to a long dataframe
+    # first, grab indicies
+    network <- as.data.frame(which(co_occur!=0,arr.ind = T))
+    df <- as.data.frame(network)
+    
+    # next, get edge weights
+    rownames(df) <- NULL
+    df$weight <- co_occur[which(co_occur!=0)]
+    
+    # make things zero indexed so there's no complaining
+    df<- df %>%
+      filter(row < col) %>%
+      mutate(row = row - 1,
+             col = col - 1)
+    
+    # and provide nice names
+    names(df) <- c("source", "target", "value")
+    
+    #mapping school names to the program codes 
+    df_mat <- data.frame(names(v),v[names(v)])
+    
+    colnames(df_mat)<-c("pgcode","value")
+    rownames(df_mat)<- NULL
+    final <- merge(df_mat,percode_schoolmapping_fuller, by ="pgcode") %>%
+       mutate(display_name=paste(as.character(pgname), " (", as.character(pgcode), ")", sep=""))
+    
+    # add node labels and sizes
+    nodes<- data.frame(name = final$display_name,
+                       group = 1,
+                       size = diag(co_occur))
+
+    code <- as.character((final %>% filter(pgcode == code))$display_name[1])
+
+    # same thing for program codes only
+    # nodes<- data.frame(name = final$pgcode,
+    #                    group = 1,
+    #                    size = diag(co_occur))
+    # 
+    nodes <- nodes %>% mutate(group = ifelse(name==code,1,2))
+    
+    # do a nicer plot that sizes nodes and weights edges
+    df <-
+      df %>%
+      filter(value>1)
+    
+    ColourScale <- 'd3.scaleOrdinal().domain(["1","2"]).range(["#FF6900","#694489"]);'
+    
+    fn <-forceNetwork(Links = df, Nodes = nodes, Source = "source",
+                      Target = "target", Value = "value", NodeID = "name", Nodesize = "size",
+                      Group = "group", opacity = 0.4, zoom = TRUE,fontSize = 32, linkDistance = 150, colourScale=JS(ColourScale),
+                      clickAction = clickJS)
+    return(fn)
+  }
+}
+
 shinyServer(function(input, output) {
 
   output$school_network <- renderForceNetwork({
-    MisNodes <-
-      MisNodes %>%
-      mutate(id=row_number()-1)
     
-    selected_school <- input$school
-    school_id <- MisNodes[which(MisNodes$name==selected_school), ]$id
-    school_group_num <- MisNodes[which(MisNodes$name==selected_school), ]$group
-    
-    # school_group_nodes <-
-    #   MisNodes %>%
-    #   filter(group==school_group_num)
-    
-    school_group_links <-
-      MisLinks %>%
-      filter(source==school_id | target==school_id)
-    
-    forceNetwork(Links = school_group_links, Nodes = MisNodes, Source = "source",
-                 Target = "target", Value = "value", NodeID = "name",
-                 Group = "group", opacity = 0.4, zoom = TRUE)
+    display_school_neighborhood(input$school)
 
+  })
+  
+  output$school_info <- renderUI({
+    code <-
+      codemap %>%
+      mutate(display_name=paste(as.character(pgname), " (", as.character(pgcode), ")", sep="")) %>%
+      filter(display_name==input$school)
+    
+    code <- as.character(code[1, "pgcode"])
+    
+    pgname <- as.character((percode_schoolmapping_fuller %>% filter(pgcode == code))$pgname[1])
+    school_name <- as.character((percode_schoolmapping_fuller %>% filter(pgcode == code))$school_name[1])
+    interest <- as.character((percode_schoolmapping_fuller %>% filter(pgcode == code))$interest[1])
+    method <- as.character((percode_schoolmapping_fuller %>% filter(pgcode == code))$method[1])
+    #size <- as.character((percode_schoolmapping_fuller %>% filter(pgcode == code))$size[1])
+    
+    nodes <- get_related_nodes(input$school)
+    input_name <- unlist(strsplit(input$school, split=' (', fixed=TRUE))[1]
+    size <- as.character((nodes %>% filter(grepl(input_name, name)))$size[1])
+
+    str1 <- paste("<b>Program Name:</b> ", pgname)
+    str2 <- paste("<b>Program Code:</b> ", code)
+    str3 <- paste("<b>School:</b> ", school_name)
+    str4 <- paste("<b>Interest Area:</b> ", interest)
+    str5 <- paste("<b>Admissions Method:</b> ", method)
+    str6 <- paste("<b>Number of Applicants in 2015:</b>", size)
+    
+    HTML(paste(str1, str2, str3, str4, str5, str6, sep='<br/>'))
+  })
+  
+  output$related_programs <- renderUI({
+    nodes <- get_related_nodes(input$school)
+    
+    final_str <- "<ol type='1'>"
+    
+    for (i in seq(nrow(nodes))) {
+      if(nodes[i, 'name']!=input$school) {
+        final_str <- paste(final_str,
+                           "<li>", 
+                           "<a href='http://schoolfinder.nyc.gov/#/schools/",
+                           as.character(nodes[i,'dbn']),
+                           "' target='_blank'>",
+                           as.character(nodes[i, 'name']),
+                           "</a></li>",
+                           sep="")
+      }
+    }
+    
+    HTML(paste(final_str, "</ol/>"))
   })
 
 })
--- shiny_apps/school_networks_shiny/ui.R
+++ shiny_apps/school_networks_shiny/ui.R
@@ -6,26 +6,60 @@
 #
 
 library(shiny)
-data(MisLinks)
-data(MisNodes)
+library(tidyverse)
+library(networkD3)
+library(htmlwidgets)
+library(readr)
+library(Matrix)
+
+load('cooccur_matrix.Rdata')
+load('schoolcodemapping_fuller.Rdata')
+codemap <- percode_schoolmapping_fuller
+school_codemap <- data.frame(codemap$pgcode,codemap$school_name)
+colnames(school_codemap) <- c("pgcode","schoolname")
+
+choices <- paste(as.character(codemap$pgname), " (", as.character(codemap$pgcode), ")", sep="")
 
 shinyUI(fluidPage(
+  
+  tags$head(
+    tags$style(HTML("hr {border-top: 1px solid #A9A9A9;}"))
+  ),
 
   # Application title
-  titlePanel("School Networks"),
-
-  # Sidebar with a slider input for number of bins
+  titlePanel("School Networks: Which Schools Are Related To Each Other?"),
+  
+  helpText(HTML("<p>Select a school program to discover similar programs.</p>
+                <p>Double-click on a node to see the network for that program.</p>
+                <p>The node size is relative to the number of applicants that apply to that school.</p>")),
+  
   sidebarLayout(
-    sidebarPanel(
-      selectizeInput("school",
-                  label="Select a character",
-                  choices = as.list(sort(MisNodes$name)))
-    ),
-
-    # Show a plot of the generated distribution
-    mainPanel(
-      
-      forceNetworkOutput("school_network")
+                sidebarPanel(
+                  
+                  selectizeInput("school",
+                                 label="Select or type a program name or program code.\n",
+                                 choices = sort(choices)),
+                  
+                  conditionalPanel(
+                    condition='input.school!=""',
+                    
+                    hr(),
+                    
+                    h4("Program Information"),
+                    htmlOutput("school_info"),
+                    
+                    br(),
+                    
+                    h4("Related Programs"),
+                    htmlOutput("related_programs")
+                  )
+                ),
+                
+          mainPanel(
+            forceNetworkOutput("school_network")
+            
+          )
+      )
     )
   )
-))
+  
